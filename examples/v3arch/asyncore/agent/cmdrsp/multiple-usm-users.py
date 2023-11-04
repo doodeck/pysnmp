cf2fb3f728ad6fd74ead355a47b61ccd6f0703c8
Krzysztof Dudkiewicz <8515450+doodeck@users.noreply.github.com>
2023-11-04 07:37:20 +0000

    Superimpose another file version from the doc/tutorial

--- examples/v3arch/asyncore/agent/cmdrsp/multiple-usm-users.py
+++ examples/v3arch/asyncore/agent/cmdrsp/multiple-usm-users.py
@@ -6,11 +6,13 @@ Listen and respond to SNMP GET/SET/GETNEXT/GETBULK queries with
 the following options:
 
 * SNMPv3
-* with USM user 'usr-md5-des', auth: MD5, priv DES or
-  with USM user 'usr-sha-none', auth: SHA, no privacy
-  with USM user 'usr-sha-aes128', auth: SHA, priv AES
+* with USM user:
+    - 'usr-md5-des', auth: MD5, priv DES or
+    - 'usr-sha-none', auth: SHA, no privacy
+    - 'usr-sha-aes128', auth: SHA, priv AES
 * allow access to SNMPv2-MIB objects (1.3.6.1.2.1)
 * over IPv4/UDP, listening at 127.0.0.1:161
+* using asyncio network transport (available since Python 3.4)
 
 Either of the following Net-SNMP commands will walk this Agent:
 
@@ -18,19 +20,26 @@ Either of the following Net-SNMP commands will walk this Agent:
 | $ snmpwalk -v3 -u usr-sha-none -l authNoPriv -a SHA -A authkey1 localhost .1.3.6
 | $ snmpwalk -v3 -u usr-sha-aes128 -l authPriv -a SHA -A authkey1 -x AES -X privkey1 localhost .1.3.6
 
+Requires Python 3.4 and later!
+
 """#
 from pysnmp.entity import engine, config
 from pysnmp.entity.rfc3413 import cmdrsp, context
-from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.carrier.asyncio.dgram import udp
+import asyncio
 
-# Create SNMP engine
+# Get the event loop for this thread
+loop = asyncio.get_event_loop()
+
+# Create SNMP engine with autogenernated engineID and pre-bound
+# to socket transport dispatcher
 snmpEngine = engine.SnmpEngine()
 
 # Transport setup
 
 # UDP over IPv4
 config.addTransport(
-    snmpEngine, udp.domainName, udp.UdpTransport().openServerMode(("127.0.0.1", 1161))
+    snmpEngine, udp.domainName, udp.UdpTransport().openServerMode(("127.0.0.1", 161))
 )
 
 # SNMPv3/USM setup
@@ -76,12 +85,5 @@ cmdrsp.SetCommandResponder(snmpEngine, snmpContext)
 cmdrsp.NextCommandResponder(snmpEngine, snmpContext)
 cmdrsp.BulkCommandResponder(snmpEngine, snmpContext)
 
-# Register an imaginary never-ending job to keep I/O dispatcher running forever
-snmpEngine.transportDispatcher.jobStarted(1)
-
-# Run I/O dispatcher which would receive queries and send responses
-try:
-    snmpEngine.transportDispatcher.runDispatcher()
-except:
-    snmpEngine.transportDispatcher.closeDispatcher()
-    raise
+# Run asyncio main loop
+loop.run_forever()
